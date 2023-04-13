e86afde83eb6cb798dd40210443bb3a0eaf8d385
Jem Chen <jem.chen@stonybrook.edu>
2023-04-13 19:06:43 -0400

    gerrychain

---
+++ scripts/gerrychain/accept.py
@@ -0,0 +1,22 @@
+from .random import random
+from gerrychain.partition import Partition
+
+
+def always_accept(partition: Partition) -> bool:
+    return True
+
+
+def cut_edge_accept(partition: Partition) -> bool:
+    """Always accepts the flip if the number of cut_edges increases.
+    Otherwise, uses the Metropolis criterion to decide.
+
+    :param partition: The current partition to accept a flip from.
+    :return: True if accepted, False to remain in place
+
+    """
+    bound = 1.0
+
+    if partition.parent is not None:
+        bound = min(1, len(partition.parent["cut_edges"]) / len(partition["cut_edges"]))
+
+    return random.random() < bound
