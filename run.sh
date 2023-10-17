7afab1bbc2e43a2c129625c789bdcde4b195f11e
zfdupont <zachary.dupont@stonybrook.edu>
2023-10-17 17:54:16 -0400

    oops 2

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ touch log
 mvn wrapper:wrapper
 ./mvnw spring-boot:run &> log &
 cd ../static/app
-if [ -z "$(ls -A /path/to/dir)" ]; then
+if [ -z "$(ls -A )" ]; then
 	git clone https://github.com/zfdupont/huskies-client.git .
 fi
 npm install --force
